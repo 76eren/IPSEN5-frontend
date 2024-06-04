18ee64eff3108fa87b82b47ebcbceb2ab3fa645e
Stan <s1136644@student.hsleiden.nl>
2024-06-04 13:06:28 +0200

    fix: add delete modal and sort reservations by date

--- src/app/reservations/delete-modal/delete-modal.component.ts
+++ src/app/reservations/delete-modal/delete-modal.component.ts
@@ -1,27 +1,42 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {ActivatedRoute, Router} from "@angular/router";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Reservation } from '../../shared/model/reservation.model';
+import { ToastrService } from 'ngx-toastr';
+import { ReservationService } from '../../shared/service/reservation.service';
+import { ReservationsComponent } from '../reservations.component';
 
 @Component({
   selector: 'app-delete-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DeleteModalComponent],
   templateUrl: './delete-modal.component.html',
-  styleUrl: './delete-modal.component.scss'
+  styleUrl: './delete-modal.component.scss',
 })
 export class DeleteModalComponent {
   @Output() closeModalEvent = new EventEmitter<void>();
-  @Input() message: string = "Weet u het zeker dat u de reservering wilt annuleren?";
+  @Input() message: string =
+    'Weet u het zeker dat u de reservering wilt annuleren?';
   @Input() deleteFunction!: () => void;
+  @Input() reservation!: Reservation;
 
-  constructor(private route: ActivatedRoute,
-              private router: Router,) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private reservationService: ReservationService,
+    private reservationComponent: ReservationsComponent
+  ) {}
 
   public closeModal() {
     this.closeModalEvent.emit();
   }
 
   deletionPressed() {
+    this.toastr.success('Reservering is geannuleerd');
+    
+    this.reservationComponent.getUpcomingReservations();
+
+    this.closeModal();
   }
 }
--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
-import {DeleteModalComponent} from "./delete-modal/delete-modal.component";
+import { Component, OnInit } from '@angular/core';
+import { DeleteModalComponent } from './delete-modal/delete-modal.component';
+import { Reservation } from '../shared/model/reservation.model';
+import { ReservationService } from '../shared/service/reservation.service';
+import { CommonModule } from '@angular/common';
+import { LucideAngularModule } from 'lucide-angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reservations',
   standalone: true,
   imports: [
-    DeleteModalComponent
+    DeleteModalComponent,
+    CommonModule,
+    LucideAngularModule,
+    RouterModule,
   ],
   templateUrl: './reservations.component.html',
-  styleUrl: './reservations.component.scss'
+  styleUrl: './reservations.component.scss',
 })
-export class ReservationsComponent {
+export class ReservationsComponent implements OnInit {
+  reservations: Reservation[] = [];
+
+  constructor(private reservationService: ReservationService) {}
+
   isDeleteModalVisible: boolean = false;
 
   openDeleteModal() {
     this.isDeleteModalVisible = !this.isDeleteModalVisible;
+  }
+
+  ngOnInit() {
+    this.getUpcomingReservations();
+  }
+
+  async getUpcomingReservations(): Promise<void> {
+    let allReservations = await this.reservationService.getAllReservations();
+    let now = new Date();
+
+    let upcomingReservations = allReservations.filter((reservation) => {
+      let startDateTime = new Date(reservation.startDateTime);
+      return startDateTime >= now;
+    });
+
+    let sortedReservations = upcomingReservations.sort((a, b) => {
+      return (
+        new Date(a.startDateTime).getTime() -
+        new Date(b.startDateTime).getTime()
+      );
+    });
+
+    this.reservations = sortedReservations;
   }
 }
