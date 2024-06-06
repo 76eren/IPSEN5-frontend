d586cb2c50bcf6ce3e908273e33b15bacf3f52bb
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2024-06-06 22:05:12 +0200

    Feat: added validation errors for chose time at Kies datum en tijd step

--- src/app/reservation/create-reservation/date-and-time-step/date-and-time-step.component.ts
+++ src/app/reservation/create-reservation/date-and-time-step/date-and-time-step.component.ts
@@ -6,6 +6,7 @@ import {provideNativeDateAdapter} from "@angular/material/core";
 import {NgxMaterialTimepickerModule, TIME_LOCALE} from "ngx-material-timepicker";
 import {FormControl, FormGroup, FormsModule, Validators} from "@angular/forms";
 import {MatInputModule} from "@angular/material/input";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-date-and-time-step',
@@ -32,7 +33,7 @@ export class DateAndTimeStepComponent {
   protected currentTime!: string;
   @Output() reservationDateAndTime = new EventEmitter<{ startDate: Date, endDate: Date }>();
 
-  constructor() {
+  constructor(private toastr: ToastrService) {
     const now = new Date();
     this.currentTime = `${now.getHours()}:${now.getMinutes()}`;
     this.dateAndTimeFormGroup = new FormGroup({
@@ -50,8 +51,19 @@ export class DateAndTimeStepComponent {
   }
 
   protected addReservationDateAndTime(): void {
+    if (!this.selectedDate || !this.startTime || !this.endTime) {
+      this.toastr.error('Alle velden zijn verplicht!', 'Validatie Error');
+      return;
+    }
+
     const startDate = this.getStartFormattedDateAndTime(this.selectedDate, this.startTime);
     const endDate = this.getStartFormattedDateAndTime(this.selectedDate, this.endTime);
+
+    if (endDate <= startDate) {
+      this.toastr.error('Eindtijd moet na starttijd zijn!', 'Validatie Error');
+      return;
+    }
+
     this.reservationDateAndTime.emit({startDate, endDate});
     this.dateAndTimeFormGroup.setValue({selectedDate: this.selectedDate, startTime: this.startTime, endTime: this.endTime});
   }
