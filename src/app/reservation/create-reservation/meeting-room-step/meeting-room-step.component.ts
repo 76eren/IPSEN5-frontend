4239903b946f24ee147c2852c86ad89804f6dba9
Yaman Kwefati <64845605+Yaman-Kwefati@users.noreply.github.com>
2024-06-11 16:05:28 +0200

    Feat: added reserve room flow

---
+++ src/app/reservation/create-reservation/meeting-room-step/meeting-room-step.component.ts
@@ -0,0 +1,102 @@
+import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+import {LocationService} from "../../../shared/service/location.service";
+import {Location} from "../../../shared/model/location.model";
+import {LocationComponent} from "../../../manage-locations/location/location.component";
+import {MatStepperNext, MatStepperPrevious} from "@angular/material/stepper";
+import {MeetingRoomUnitComponent} from "./meeting-room-unit/meeting-room-unit.component";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ToastrService} from "ngx-toastr";
+import {MatPaginatorModule, PageEvent} from "@angular/material/paginator";
+
+@Component({
+  selector: 'app-meeting-room-step',
+  standalone: true,
+  imports: [
+    LocationComponent,
+    MatStepperNext,
+    MatStepperPrevious,
+    MeetingRoomUnitComponent,
+    MatPaginatorModule,
+  ],
+  templateUrl: './meeting-room-step.component.html',
+  styleUrl: './meeting-room-step.component.scss'
+})
+export class MeetingRoomStepComponent implements OnInit{
+  roomsFormGroup: FormGroup;
+  protected meetingRooms: Location[] = [];
+  protected selectedLocation!: Location;
+  protected canContinue = false;
+  @Input() buildingId!: string;
+  @Input() numberOfPersons!: number;
+  @Input() startDate!: Date;
+  @Input() endDate!: Date;
+  @Output() selectedMeetingRoom = new EventEmitter<Location>();
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  displayedMeetingRooms!: Location[];
+
+  constructor(private locationService: LocationService,
+              private toastr: ToastrService){
+    this.roomsFormGroup = new FormGroup({
+      selectedMeetingRoom: new FormControl(null, Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  handlePage(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    const startIndex = e.pageIndex * e.pageSize;
+
+    this.displayedMeetingRooms = this.meetingRooms.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['endDate'] && changes['endDate'].currentValue) {
+      this.locationService.getAvailableMeetingRooms({
+        buildingId: this.buildingId,
+        numberOfPeople: this.numberOfPersons,
+        startDateTime: this.formatDateTime(this.startDate),
+        endDateTime: this.formatDateTime(this.endDate)
+      }).subscribe(
+        data => {
+          this.meetingRooms = data.payload;
+          this.displayedMeetingRooms = this.meetingRooms.slice(0, this.pageSize);
+        }
+      );
+    }
+  }
+  protected formatDate(date: Date): string {
+    return date.toISOString().slice(0,10);
+  }
+
+  protected formatTime(date: Date): string {
+    return date.toISOString().slice(11,19);
+  }
+  protected formatDateTime(date: Date): string {
+    return this.formatDate(date) + 'T' + this.formatTime(date);
+  }
+
+  protected addMeetingRoom(location: Location){
+    this.selectedMeetingRoom.emit(location);
+    this.roomsFormGroup.get('selectedMeetingRoom')?.setValue(location);
+  }
+
+  protected toggleSelected(location: Location) {
+    for (let meetingRoom of this.meetingRooms) {
+      meetingRoom.isActivated = false;
+    }
+    location.isActivated = !location.isActivated;
+    this.selectedLocation = location;
+  }
+
+  protected checkMeetingRoom() {
+    if (!this.selectedLocation) {
+      this.toastr.error('Selecteer een vergaderruimte!', 'Validatie Error');
+      return;
+    }
+    this.addMeetingRoom(this.selectedLocation);
+  }
+}
--- src/app/reservation/create-reservation/verify-reservation-step/verify-reservation-step.component.ts
+++ src/app/reservation/create-reservation/verify-reservation-step/verify-reservation-step.component.ts
@@ -1,13 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {ReservationService} from "../../../shared/service/reservation.service";
-import {Reservation} from "../../../shared/model/reservation.model";
-import {string} from "zod";
 import {Wing} from "../../../shared/model/wing.model";
 import {Floor} from "../../../shared/model/floor.model";
 import {Building} from "../../../shared/model/building.model";
 import {ReservationType} from "../../../shared/model/reservering-type.enum";
 import {DatePipe, NgIf} from "@angular/common";
 import {Router} from "@angular/router";
+import {Location} from "../../../shared/model/location.model";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-verify-reservation-step',
@@ -21,32 +21,42 @@ import {Router} from "@angular/router";
 })
 export class VerifyReservationStepComponent {
   @Input() building!: Building;
-  @Input() floor!: Floor;
-  @Input() wing!: Wing;
+  @Input() floor?: Floor;
+  @Input() wing?: Wing;
   @Input() startDate = new Date();
   @Input() endDate = new Date();
   @Input() reservationType!: ReservationType;
+  @Input() location!: Location;
+  @Input() numberOfPersons!: number;
 
   isLoading = false;
 
   constructor(private reservationService: ReservationService,
-              private router: Router){
+              private router: Router,
+              private toastr: ToastrService) {
   }
 
-  formatDate(date: Date): string {
+  protected formatDate(date: Date): string {
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
   }
 
-  formatTime(date: Date): string {
+  protected formatTime(date: Date): string {
     const hours = date.getHours();
     const minutes = String(date.getMinutes()).padStart(2, '0');
     return `${hours}:${minutes}`;
   }
 
-  makeReservation(): void {
+  protected makeReservation(): void {
     this.isLoading = true;
-    this.reservationService.makeReservation({
-      wingId: this.wing.id,
+    if (this.reservationType == ReservationType.FLEXPLEK){
+      this.makeWorkplaceReservation();
+    }
+    this.makeMeetingRoomReservation();
+  }
+
+  protected makeWorkplaceReservation(): void {
+    this.reservationService.makeWorkplaceReservation({
+      wingId: this.wing!.id,
       startDateTime: this.startDate,
       endDateTime: this.endDate
     }).subscribe(
@@ -56,8 +66,28 @@ export class VerifyReservationStepComponent {
       },
       error => {
         this.isLoading = false;
-        // handle error here
+        this.toastr.error('Er is iets misgegaan bij het maken van de reservering', 'Fout');
       }
     );
   }
+
+  protected makeMeetingRoomReservation(): void {
+    this.reservationService.makeMeetingRoomReservation({
+      locationId: this.location.id,
+      startDateTime: this.startDate,
+      endDateTime: this.endDate,
+      numberOfAttendees: this.numberOfPersons
+    }).subscribe(
+      data => {
+        this.isLoading = false;
+        this.router.navigate(['/create-reservation/success']);
+      },
+      error => {
+        this.isLoading = false;
+        this.toastr.error('Er is iets misgegaan bij het maken van de reservering', 'Fout');
+      }
+    );
+  }
+
+  protected readonly ReservationType = ReservationType;
 }
