bdb0f7eb9239cfdff500c00f791ffe920ea202d8
isabelhb <117602528+isabelhb@users.noreply.github.com>
2024-06-15 16:16:29 +0200

    FEAT: cypress tests for fetching userdata and favorite colleague data

--- cypress/e2e/favorite-colleague.component.cy.ts
+++ cypress/e2e/favorite-colleague.component.cy.ts
@@ -7,10 +7,36 @@ describe('favorite-colleagues', () => {
 
 
   context('happy-flow', () => {
-    it('should navigate to favorites page', () => {
-      cy.visit('http://localhost:4200/#/favorites');
+      beforeEach(function () {
+        cy.intercept('GET', '/api/v1/user', {fixture: 'authenticated.fixture.json'})
+        cy.intercept('GET', "/api/v1/user",
+          {fixture: 'favorite-colleague-fixtures/get-users.fixture.json'}).as('getUsers');
+        cy.intercept('GET', "/api/v1/user/favorite-colleagues",
+          {fixture: 'favorite-colleague-fixtures/get-favorites.fixture.json'}).as('getFavorites');
 
-      cy.wait(300)
+        cy.visit('http://localhost:4200/#/favorites');
+        cy.wait(500)
+      })
+
+    it('should navigate to favorites page', () => {
+      cy.url().should('contain', '/favorites')
     })
+
+    it('should fetch userdata on favorites page', () => {
+      cy.wait(['@getUsers'])
+        .then(([users]) => {
+        expect(users.response?.statusCode).to.equal(200);
+
+      });
+    })
+
+    it('should fetch favorite colleagues of user', () => {
+      cy.wait(['@getFavorites'])
+        .then(([favorites]) => {
+          expect(favorites.response?.statusCode).to.equal(200);
+
+        });
+    })
+
   })
 })
